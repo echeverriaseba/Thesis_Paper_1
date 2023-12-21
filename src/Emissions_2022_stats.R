af3e01fb747fa06221d3bf6ebdd3f2f6f2c8ef48
echeverriaseba <secheverriap@gmail.com>
2023-12-21 11:27:36 +0100

    Working Emission stats

--- src/Emissions_2022_stats.R
+++ src/Emissions_2022_stats.R
@@ -59,3 +59,67 @@ boxplot(Master_GHG_2022_no_NA$Water_level_corr)
 boxplot(Master_GHG_2022_no_NA$Water_level_corr)$out
 max(boxplot(Master_GHG_2022_no_NA$Water_level_corr)$out) # prints the maximum outlier value 
 min(boxplot(Master_GHG_2022_no_NA$Water_level_corr)$out) # prints the minimum outlier value
+
+summary(Master_GHG_2022_no_NA$Conduct_microS_cm)
+boxplot(Master_GHG_2022_no_NA$Conduct_microS_cm)
+boxplot(Master_GHG_2022_no_NA$Conduct_microS_cm)$out
+max(boxplot(Master_GHG_2022_no_NA$Conduct_microS_cm)$out) # prints the maximum outlier value 
+min(boxplot(Master_GHG_2022_no_NA$Conduct_microS_cm)$out) # prints the minimum outlier value
+## Outliers identified for var: Conduct_microS_cm - 0.46 and 2.24
+
+summary(Master_GHG_2022_no_NA$Temp_10_cm)
+boxplot(Master_GHG_2022_no_NA$Temp_10_cm)
+boxplot(Master_GHG_2022_no_NA$Temp_10_cm)$out
+max(boxplot(Master_GHG_2022_no_NA$Temp_10_cm)$out) # prints the maximum outlier value 
+min(boxplot(Master_GHG_2022_no_NA$Temp_10_cm)$out) # prints the minimum outlier value
+
+summary(Master_GHG_2022_no_NA$pH_soil)
+boxplot(Master_GHG_2022_no_NA$pH_soil)
+boxplot(Master_GHG_2022_no_NA$pH_soil)$out
+max(boxplot(Master_GHG_2022_no_NA$pH_soil)$out) # prints the maximum outlier value 
+min(boxplot(Master_GHG_2022_no_NA$pH_soil)$out) # prints the minimum outlier value
+
+summary(Master_GHG_2022_no_NA$Redox_pot)
+boxplot(Master_GHG_2022_no_NA$Redox_pot)
+boxplot(Master_GHG_2022_no_NA$Redox_pot)$out
+max(boxplot(Master_GHG_2022_no_NA$Redox_pot)$out) # prints the maximum outlier value 
+min(boxplot(Master_GHG_2022_no_NA$Redox_pot)$out) # prints the minimum outlier value
+
+summary(Master_GHG_2022_no_NA$Water_temp)
+boxplot(Master_GHG_2022_no_NA$Water_temp)
+boxplot(Master_GHG_2022_no_NA$Water_temp)$out
+max(boxplot(Master_GHG_2022_no_NA$Water_temp)$out) # prints the maximum outlier value 
+min(boxplot(Master_GHG_2022_no_NA$Water_temp)$out) # prints the minimum outlier value
+
+summary(Master_GHG_2022_no_NA$O2_percent)
+boxplot(Master_GHG_2022_no_NA$O2_percent)
+boxplot(Master_GHG_2022_no_NA$O2_percent)$out
+max(boxplot(Master_GHG_2022_no_NA$O2_percent)$out) # prints the maximum outlier value 
+min(boxplot(Master_GHG_2022_no_NA$O2_percent)$out) # prints the minimum outlier value
+
+summary(Master_GHG_2022_no_NA$O2_mg_l)
+boxplot(Master_GHG_2022_no_NA$O2_mg_l)
+boxplot(Master_GHG_2022_no_NA$O2_mg_l)$out
+max(boxplot(Master_GHG_2022_no_NA$O2_mg_l)$out) # prints the maximum outlier value 
+min(boxplot(Master_GHG_2022_no_NA$O2_mg_l)$out) # prints the minimum outlier value
+
+summary(Master_GHG_2022_no_NA$Salinity)
+boxplot(Master_GHG_2022_no_NA$Salinity)
+boxplot(Master_GHG_2022_no_NA$Salinity)$out
+max(boxplot(Master_GHG_2022_no_NA$Salinity)$out) # prints the maximum outlier value 
+min(boxplot(Master_GHG_2022_no_NA$Salinity)$out) # prints the minimum outlier value
+## Outliers identified for var: Salinity - 0.37 and 1.11
+
+summary(Master_GHG_2022_no_NA$pH_water)
+boxplot(Master_GHG_2022_no_NA$pH_water)
+boxplot(Master_GHG_2022_no_NA$pH_water)$out
+max(boxplot(Master_GHG_2022_no_NA$pH_water)$out) # prints the maximum outlier value 
+min(boxplot(Master_GHG_2022_no_NA$pH_water)$out) # prints the minimum outlier value
+
+## From here onwards tests and models will be evaluated both considering outliers and removing them (suffix: "_nooutliers")
+
+Master_GHG_2022_no_NA_nooutliers <-  Master_GHG_2022_no_NA %>%  # New data frame with a (1/0) Outliers column, then removing outlier rows
+  mutate(Outliers =case_when(Temp_soil > 37  | Temp_soil < 5 | Conduct_microS_cm < 0.5 |  Conduct_microS_cm > 2.2 | Env_temp_initial > 230 | 
+                               Salinity < 0.4 | Salinity > 1.1 ~ 1,TRUE ~ 0)) %>% 
+                               filter(Outliers == 0)
+
